c05d668a7655ca021c537f2a360cd096348466ca
iurigiroldi17 <iuri241206@edu.unipar.br>
2024-08-21 20:46:58 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 17;
+        String nomePessoa = "Iuri";
+
+
+       /*
+       * > Maior
+       * >= Maior igual
+       * < Menor
+       * <= Menor igual
+       * == é igual
+       * != é diferente
+       * equal compara Textos
+       *
+        */
+
+        if(idadePessoa > 17) {
+            System.out.println("é maior que 17");
+
+        }else{
+            System.out.println("Não é maior");
+        }
     }
 }
